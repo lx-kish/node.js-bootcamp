a0f13fdbe066dd5f119ed48ece9d1d1abc83d045
lx kish <urban.ktulhu@gmail.com>
2020-08-21 20:33:42 +1200

    feat(core): add users routes, split routes and middleware

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+
+//MIDDLEWARES
+
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log('Hello from the middleware!');
+    next();
+});
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//ROUTE HANDLERS
+
 const getAllTours = (req, res) => {
+    console.log(req.requestTime);
     res.status(200).json({
         status: 'success',
         results: tours.length,
@@ -91,11 +109,47 @@ const deleteTour = (req, res) => {
     })
 };
 
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
+const getAllUsers = (req, res) => {
+
+    res.status(500).json({
+        status: 'error',
+        message: 'This route has not implemented yet'
+    })
+};
+
+const createUser = (req, res) => {
+
+    res.status(500).json({
+        status: 'error',
+        message: 'This route has not implemented yet'
+    })
+};
+
+const getUser = (req, res) => {
+
+    res.status(500).json({
+        status: 'error',
+        message: 'This route has not implemented yet'
+    })
+};
+
+const updateUser = (req, res) => {
+
+    res.status(500).json({
+        status: 'error',
+        message: 'This route has not implemented yet'
+    })
+};
+
+const deleteUser = (req, res) => {
+
+    res.status(500).json({
+        status: 'error',
+        message: 'This route has not implemented yet'
+    })
+};
+
+//ROUTES
 
 app
     .route('/api/v1/tours')
@@ -107,6 +161,19 @@ app
     .get(getTour)
     .patch(updateTour)
     .delete(deleteTour);
+
+app
+    .route('/api/v1/users')
+    .get(getAllUsers)
+    .post(createUser);
+
+app
+    .route('/api/v1/users/:id')
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser);
+
+// SEREVR START
 
 const port = 5050;
 app.listen(port, () => {
