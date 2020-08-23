eac95e43237b7c46e9471a082c8713c818de183a
lx kish <urban.ktulhu@gmail.com>
2020-08-24 00:53:44 +1200

    feat(core): add module for import and delete data in the DB

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+mongoose.connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true
+}).then(() => console.log('DB connection successfully established'));
+
+//read JSON file
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+
+//import data into DB
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data has been loaded successfully');
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+//delete data from collection
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data has been deleted successfully');
+        process.exit();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+if(process.argv[2] === '--import') {
+    importData();
+}
+if(process.argv[2] === '--delete') {
+    deleteData();
+}
