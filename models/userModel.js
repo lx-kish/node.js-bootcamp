6ff74730d655b1b595f6acf981c1ef73fe622633
lx kish <urban.ktulhu@gmail.com>
2020-08-26 22:07:01 +1200

    feat(core): add user's sign up and sign in routes

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -26,16 +27,37 @@ const userSchema = new mongoose.Schema({
             true,
             'Please provide a password'
         ],
-        minlength: 8
+        minlength: 8,
+        select: false
     },
     passwordConfirm: {
         type: String,
         required: [
             true,
             'Please provide a password'
-        ]
+        ],
+        validate: {
+            // This validator works on CREATE or SAVE only!
+            validator: function(el) {
+                return el === this.password;
+            },
+            message: 'Password confirm should be the same as a password'
+        }
     }
 });
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+
+    this.password = await bcrypt.hash(this.password, 12);
+
+    this.passwordConfirm = undefined;
+    next();
+});
+
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model('User', userSchema);
 
