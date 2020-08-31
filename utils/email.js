e1ee01c31534abc90daba710f6ca1e7ea6e8167e
lx kish <urban.ktulhu@gmail.com>
2020-08-31 20:51:27 +1200

    fix(core): correct emailer

---
+++ utils/email.js
@@ -0,0 +1,27 @@
+const nodemailer = require('nodemailer');
+
+module.exports = async options => {
+    // 1) Create a transporter
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+        }
+        //activate in gmail 'less secure app' option
+    })
+
+    // 2) Define the email options
+    const mailOptions = {
+        from: 'John Doe <email@mail.com>',
+        to: options.email,
+        subject: options.subject,
+        text: options.message
+        // html:
+
+    }
+
+    // 3) Send the email
+    await transporter.sendMail(mailOptions);
+}
