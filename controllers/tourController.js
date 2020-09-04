0089fd404f64cf77943b5bb8d9c7512d55cfec41
lx kish <urban.ktulhu@gmail.com>
2020-09-05 09:26:22 +1200

    feat(core): add indexes for improving request performance

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,5 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
 exports.aliasTopTours = async (req, res, next) => {
