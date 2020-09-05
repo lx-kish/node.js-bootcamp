bed4ae78d27b18e0cf56f188d1d0891386e9e272
lx kish <urban.ktulhu@gmail.com>
2020-09-06 00:24:36 +1200

    feat(core): add searching tours by distance using geospatial queries

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -28,7 +28,9 @@ router
         tourController.getMonthlyPlan
     );
 
-router.route('/tours-within/:distance/center/:latlng/unit/:unit', tourController.getToursWithin);
+router
+    .route('/tours-within/:distance/center/:latlng/unit/:unit')
+    .get(tourController.getToursWithin);
 
 router
     .route('/')
