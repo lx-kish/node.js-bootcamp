62ae4f7d04152c5a7925aa126a5a89f468d66518
lx kish <urban.ktulhu@gmail.com>
2020-09-07 14:17:39 +1200

    feat(core): build appropriate architecture for MVC pattern with viewRouter and viewController

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -51,7 +52,14 @@ app.use(xss());
 
 // Prevents parameter pollution
 app.use(hpp({
-    whitelist: ['duration']
+    whitelist: [
+        'duration',
+        'ratingsQuantity',
+        'ratingsAverage',
+        'maxGroupSize',
+        'difficulty',
+        'price'
+    ]
 }));
 
 // Test middleware
@@ -62,24 +70,7 @@ app.use((req, res, next) => {
 });
 
 //ROUTES
-app.get('/', (req, res) => {
-    res.status(200).render('base', {
-        tour: 'The Forest Hicker',
-        user: 'Jonas'
-    });
-});
-
-app.get('/overview', (req, res) => {
-    res.status(200).render('overview', {
-        title: 'All Tours'
-    });
-});
-
-app.get('/tour', (req, res) => {
-    res.status(200).render('tour', {
-        title: 'The Forest Hiker Tour'
-    });
-});
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
